a65cde248083c848f9008886ac121f5b27ec1e06
Gabriel Lucas <gabriellucas2301@gmail.com>
2024-05-10 11:14:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,155 @@
+// Buttons Light-dark
+const btnLight = document.querySelector(".btn-light");
+const btnDark = document.querySelector(".btn-dark");
+const btnPortifolio = document.getElementById("btn-portifolio");
+const btnSkills = document.getElementById("btn-skills");
+const body = document.getElementById("body");
+const containerButtons = document.getElementById("buttons-project");
+const progressBars = document.querySelectorAll(".progress-bar");
+
+// Carregamento da Pagina
+
+this.window.addEventListener("laod", () => {
+  document.getElementById("image-skills").classList.add("hidden");
+});
+
+// Botão Dark
+
+// Botão Dark
+btnDark.addEventListener("click", () => {
+  btnLight.classList.remove("hidden");
+  btnDark.classList.add("hidden");
+  body.classList.add("toWhite");
+  body.classList.remove("toBlack");
+
+  document.getElementById("title-header").style.color = "#3d3d3d";
+  document.getElementById("profession").style.color = "#3d3d3d";
+  document.getElementById("pessoal-description-1").style.color = "#3d3d3d";
+  document.getElementById("pessoal-description-2").style.color = "#3d3d3d";
+  document.getElementById("pessoal-description-3").style.color = "#3d3d3d";
+
+  document.getElementById("number-description-1").style.color = "#3d3d3d";
+  document.getElementById("number-description-2").style.color = "#3d3d3d";
+  document.getElementById("number-description-3").style.color = "#3d3d3d";
+});
+
+// Botão Light
+
+btnLight.addEventListener("click", () => {
+  btnLight.classList.add("hidden");
+  btnDark.classList.remove("hidden");
+  body.classList.add("toBlack");
+  body.classList.remove("toWhite");
+
+  document.getElementById("title-header").style.color = "#e5e5e5";
+  document.getElementById("profession").style.color = "#e5e5e5";
+  document.getElementById("pessoal-description-1").style.color = "#e5e5e5";
+  document.getElementById("pessoal-description-2").style.color = "#e5e5e5";
+  document.getElementById("pessoal-description-3").style.color = "#e5e5e5";
+
+  document.getElementById("number-description-1").style.color = "#e5e5e5";
+  document.getElementById("number-description-2").style.color = "#e5e5e5";
+  document.getElementById("number-description-3").style.color = "#e5e5e5";
+});
+
+// Botão de Portifolio
+btnPortifolio.addEventListener("click", () => {
+  btnPortifolio.classList.add("active-btn");
+  btnSkills.classList.remove("active-btn");
+  document.getElementById("image-project").classList.remove("hidden");
+  document.getElementById("image-skills").classList.add("hidden");
+});
+
+// Botão de Skill
+
+btnSkills.addEventListener("click", () => {
+  btnSkills.classList.add("active-btn");
+  btnPortifolio.classList.remove("active-btn");
+  document.getElementById("image-project").classList.add("hidden");
+  document.getElementById("image-skills").classList.remove("hidden");
+});
+
+// Barra de Carregamento
+
+progressBars.forEach((bar) => {
+  const width = bar.getAttribute("data-width");
+  bar.style.width = width;
+});
+
+// links
+
+document.getElementById("git-link").addEventListener("click", () => {
+  window.open("https://github.com/DevGabriel0402", "_blank");
+});
+
+document.getElementById("x-link").addEventListener("click", () => {
+  window.open("https://twitter.com/gabriell_Lav", "_blank");
+});
+
+document.getElementById("linkedin-link").addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/gabriel-lucas-vieira-2384212a4/", "_blank");
+});
+
+document.getElementById("youtube-link").addEventListener("click", () => {
+  document.getElementById("soap-youtube").classList.remove("hidden");
+  document.getElementById("soap-youtube").style.animation = "toDown 0.5s forwards";
+
+  setTimeout(() => {
+    document.getElementById("soap-youtube").style.animation = "toUpIn 0.5s forwards";
+  }, 3000);
+});
+
+// Função para mostrar os botões da imagem no hover
+function adicionarEventosMouse(imagemId, botaoId, textId, backgroundId) {
+  const imagem = document.getElementById(imagemId);
+  const botao = document.getElementById(botaoId);
+  const textImg = document.getElementById(textId);
+  const background = document.getElementById(backgroundId);
+
+  imagem.addEventListener("mouseenter", () => {
+    botao.classList.add("flex");
+    botao.style.animation = "toUp 0.3s forwards";
+    textImg.style.animation = "opacityIn 0.3s forwards";
+    background.style.animation = "opacityIn 0.3s forwards";
+  });
+
+  imagem.addEventListener("mouseleave", () => {
+    botao.style.animation = "toUpReverse 0.3s forwards";
+    textImg.style.animation = "opacityOut 0.8s forwards";
+    background.style.animation = "opacityOut 0.8s forwards";
+
+    setTimeout(() => {
+      botao.classList.remove("flex");
+    }, 100);
+  });
+}
+
+adicionarEventosMouse("img-1", "button-1", "title-img-1", "background-gradient-1");
+
+adicionarEventosMouse("img-2", "button-2", "title-img-2", "background-gradient-2");
+
+adicionarEventosMouse("img-3", "button-3", "title-img-3", "background-gradient-3");
+
+adicionarEventosMouse("img-4", "button-4", "title-img-4", "background-gradient-4");
+
+adicionarEventosMouse("img-5", "button-5", "title-img-5", "background-gradient-5");
+
+adicionarEventosMouse("img-6", "button-6", "title-img-6", "background-gradient-6");
+
+document.getElementById("button-download").addEventListener("click", () => {
+  document.getElementById("btn-download-icon").classList.add("hidden");
+  document.getElementById("button-download").style.border = "1px solid #f7d039";
+  document.getElementById("icon-download").style.animation = "toLeft 0.7s forwards";
+  document.getElementById("bar-download").style.animation = "download 4s forwards";
+  document.querySelector(".bar-download").classList.remove("hidden");
+  document.getElementById("button-download").style.backgroundColor = "#fff";
+
+  setTimeout(() => {
+    document.querySelector(".bar-download").classList.add("hidden");
+    document.getElementById("bar-download").style.animation = "";
+    document.getElementById("button-download").style.backgroundColor = "#f7d039";
+    document.getElementById("btn-download-icon").classList.add("hidden");
+    document.getElementById("btn-download-icon").classList.remove("hidden");
+    window.open("./assets/Curriculo - Gabriel Lucas.pdf", "_blank");
+  }, 4000);
+});
---
+++ readme.md
@@ -0,0 +1,46 @@
+# Portfólio - Gabriel Lucas
+
+<img src="./assets/readme-img/portifolio.png">
+
+![html](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![css](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![JS](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+Este é meu portfólio de desenvolvimento onde você encontrará minhas principais redes, projetos e habilidades.
+
+## Como Acessar
+
+- **[Portfólio](https://example.com)**: Confira meus projetos.
+
+## Projetos
+
+Aqui estão alguns dos meus projetos destacados:
+
+1. **Burger**
+
+   - [Ver](https://menudigitalio.netlify.app)
+   - [GitHub](https://github.com/DevGabriel0402/cardapio-digital.io)
+
+2. **Login**
+
+   - [Ver](https://loginpage-io.netlify.app)
+   - [GitHub](https://github.com/DevGabriel0402/Tela-de-Login)
+
+3. **Preset**
+
+   - [Ver](https://preset-io.netlify.app)
+   - [GitHub](https://github.com/DevGabriel0402/presets-io)
+
+4. **Convencily Test**
+
+   - [Ver](https://convencily-test-frontend.netlify.app/)
+   - [GitHub](https://github.com/DevGabriel0402/convencily-teste-frontend)
+
+5. **Bio Link**
+
+   - [Ver](https://linksaqui.netlify.app)
+   - [GitHub](https://github.com/DevGabriel0402/linksUteis)
+
+6. **Previsão do Tempo**
+   - [Ver](https://previsaodotempo-io.netlify.app)
+   - [GitHub](https://github.com/DevGabriel0402/WeatherApp-io)
